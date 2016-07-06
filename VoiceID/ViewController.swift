fdbddf57d6e9242fe61603843f9477d3562f1484
Xiao Zheng <yjzhengxiao@126.com>
2016-07-06 14:20:08 -0700

    adding all the basic functionality

--- VoiceID/ViewController.swift
+++ VoiceID/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
 
-    override func viewDidLoad() {
+    let synth = AVSpeechSynthesizer()
+    var myUtterance = AVSpeechUtterance(string: "")
+    
+//    var rate: Float!
+//    
+//    var pitch: Float!
+//    
+//    var volume: Float!
+    
+//    func registerDefaultSettings() {
+//        rate = AVSpeechUtteranceDefaultSpeechRate
+//        pitch = 1.0
+//        volume = 1.0
+//        
+//        let defaultSpeechSettings: Dictionary<NSObject, AnyObject> = ["rate": rate, "pitch": pitch, "volume": volume]
+//        
+//        //NSUserDefaults.standardUserDefaults().registerDefaults(defaultSpeechSettings)
+//    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func textToSpeech(sender: AnyObject) {
+        
+        myUtterance = AVSpeechUtterance(string: textView.text)
+        myUtterance.rate = 0.5
+        myUtterance.pitchMultiplier = 0.25
+        synth.speakUtterance(myUtterance)
+    }
 
 }
 
